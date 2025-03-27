b8048b7163931a895fe2ec75e60b56c9a67dd9a5
PremanshBehl <premanshbehl978@gmail.com>
2025-03-28 00:07:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// Event listner --> jispe click karoge wo element par event raise hoga, aur event listener naa milne par event element ke parent par listener dhundega, waha bhi naa milne par evaent parent ke parent ke parent pe listner dhundega
+
+
+function makeBubble(){
+    let clutter ="";
+
+for(let i=1;i<=150;i++){
+    let rn = Math.floor(Math.random()*10)
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbottom").innerHTML = clutter;
+}
+
+let timer = 60;
+function runTimer(){
+    let timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+        document.querySelector("#Timerval").textContent = timer;
+        }
+        else{
+            document.querySelector("pbottom").innerHTML = `<h1>Game Over!</h1>`;
+            clearInterval(timerint);
+        }
+        
+    }, 1000);
+}
+let hitrn = 0;
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+let score=0;
+function increaseScore(){
+    score+=10;
+    document.querySelector("#scoreval").textContent = score
+}
+
+document.querySelector("#pbottom")
+.addEventListener("click",function(dets){
+    let clickednum = (Number(dets.target.textContent))
+    if(clickednum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+
+})
+
+// document.querySelector("#pbotton")
+// .addEventListener("click",function(dets){
+//     let
+// })
+
+runTimer();
+makeBubble();
+getNewHit();
